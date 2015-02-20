09e7c82c337092e6a7b204ff6183d92537d2c250
Saarots <eiristh@stud.ntnu.no>
2015-02-20 17:35:24 +0100

    Update Procespair.go
    Komprimert og fjernet en del. Printing og overføring av i fungere fortsatt ikke....

--- 6/Procespair.go
+++ 6/Procespair.go
@@ -8,21 +8,14 @@ import (
 	"encoding/json"
 )
 
-
 var i = 0
 var masterMode = 0
-
-var timeout = make(chan int, 1) 
-var number = make(chan int, 1)
 
 type Data struct{
 	Message string
 	Number int
 }
-
-
-
-func sendNumber() {
+func master() {
 	sendAddr, err := ResolveUDPAddr("udp", ":31004") // bordcaster
 	if err != nil{
 		Println("Resolve error during send")
@@ -30,73 +23,50 @@ func sendNumber() {
 	}
 	con, _ := DialUDP("udp", nil, sendAddr) // lager socket til sending
 	defer con.Close()// stenger porten etter funksjonene
-
-	<- number
+	i++
+	Println("Jeg er i MasterMode!\ni = ",i)
 	dataSend := Data{
 		Message: "Alive",
 		Number: i,
 	}
-	
 	messageSend, _ := json.Marshal(dataSend)
-	os.Stdout.Write(messageSend)	
-	number <- 1
 	Fprintf(con, string(messageSend))// Triks for å sende til UDP(tar KUN string)
-		Println("\nMelding sendt")
-
-
+	Println("\nMelding sendt")
+	time.Sleep(1*time.Second)
 }
-
 func slave() {
-	Println("I am in SlaveMode")
+	Println("I am in SlaveMode!\ni:  %v", i)
 	udpAddr, err := ResolveUDPAddr("udp", ":31004")
 	if err != nil{
 		Println("Resolve error during listen procedure")
 		os.Exit(1)
 	}
-	
 	ln, err := ListenUDP("udp", udpAddr) //lager socket for høring
 	if err != nil{
 		Println("Listening error")
 		os.Exit(1)
 	}
-
-	Println("Hører etter beskjed")
-	
 	for{
-		Println("Starter å høre")
 		ln.SetReadDeadline(time.Now().Add(3*time.Second))
 		buffer := make([]byte, 1024)
 		n, err := ln.Read(buffer) // test "_" på de andre etter på for de vi ikke trenger
 		if err != nil{
 			masterMode = 1
-			Println("I am Master:", masterMode)
+			Println("I am Master now!!!!")
 			ln.Close()
 			break
 		}
-		Println("Printer ut beskjed")
 		var messageRecv []Data
 		json.Unmarshal(buffer[0:n], &messageRecv) // lager i ny struct så er det bare å hente fra den
-		Println("From address: ", addr, "\nMessage: ", messageRecv)
+		//msg := string(messageRecv)
+		Println("Message recived is: %v", messageRecv)
 		// printer ikke rett så må få endra det!!!!
 		//i = messageRecv
-		
 	}
 }
 
-func master(){
-	<- number
-	i++
-	number <- i
-	sendNumber()
-	Println("Jeg er i MasterMode!\ni = ",i)
-	time.Sleep(1*time.Second)
-}
-
-
 func main() {
 	masterMode = 0
-	number <- 1
-
 	for{
 		if masterMode == 1{
 			master()
